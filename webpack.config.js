12d550af1701f6e4d4c8882f54d314fddd54a600
Slav <shutrow@rambler.ru>
2021-07-21 01:20:23 +0300

    4th lesson done

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
 const path = require('path')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './index.js'
+    main: './index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  } 
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+  ],
 }
