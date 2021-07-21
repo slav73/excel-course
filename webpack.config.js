de1ed274a348b5831afbe7fe5ff6e97b8e32a692
Slav <shutrow@rambler.ru>
2021-07-21 11:06:39 +0300

    5th lesson done

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './index.js',
+    main: ['@babel/polyfill', './index.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
