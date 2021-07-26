70c19264cfab4ebce147c31da854295b4ca79034
Slav <shutrow@rambler.ru>
2021-07-26 15:17:50 +0300

    Resizeof row done

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -22,7 +22,7 @@ function createRow(index, content) {
     ? '<div class="row-resize" data-resize="row"></div>'
     : ''
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
       <div class="row-info">
         ${index ? index : ''}
         ${resizer}
